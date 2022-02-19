b7256fc7a49fed26cc12e92523bb2277968f78bc
evsem <frukwork@gmail.com>
2022-02-19 12:57:28 +0300

    UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Style/App.css'
+import ButtonGrey from './UI/ButtonGrey/ButtonGrey'
+import InputGrey from './UI/InputGrey/InputGrey'
+import SelectGrey from './UI/SelectGrey/SelectGrey'
 
 const App = () => {
   return <div className="App"></div>
---
+++ src/UI/SelectGrey/SelectGrey.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './SelectGrey.module.css'
+
+const SelectGrey = () => {
+  return <select className={classes.selectWrapper}></select>
+}
+
+export default SelectGrey
